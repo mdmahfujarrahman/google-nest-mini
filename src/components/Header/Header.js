e0866cb1065b3f1cd3d52668d08fa5a6426fd86f
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-04-04 00:55:49 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React from "react";
+import CustomLink from "../CustomLink/CustomLink";
+import "./Header.css";
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className="flex align-center">
+            <h5 className="text-3xl w-40 font-bold brand-color">Nest Mini</h5>
+            <div className="flex justify-between items-center ml-6 w-full">
+                <div className="flex space-x-14">
+                    <CustomLink className="text-xl" to="">
+                        Home
+                    </CustomLink>
+                    <CustomLink className="text-xl" to="">
+                        Review
+                    </CustomLink>
+                    <CustomLink className="text-xl" to="">
+                        Shop
+                    </CustomLink>
+                    <CustomLink className="text-xl" to="">
+                        Blog
+                    </CustomLink>
+                </div>
+                <div>
+                    <CustomLink className="text-xl" to="">
+                        Dashboard
+                    </CustomLink>
+                </div>
+            </div>
+        </nav>
     );
 };
 
