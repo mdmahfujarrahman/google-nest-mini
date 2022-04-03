bbf8c364942df9225b47a4d5b460f4b42b162aa3
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-04-04 02:18:59 +0600

    custom hooks added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import BackgroundVideo from "../../video/background.mp4";
 
 const Home = () => {
     return (
-        <>
+        <div>
             <section className="banner-container items-center mt-16">
                 <div>
                     <h1 className="text-center text-4xl font-bold brand-color">
@@ -15,19 +15,24 @@ const Home = () => {
                     </h1>
                     <p className="text-center mt-6 brand-color">
                         Meet the new Nest Mini. Your music sounds bigger and
-                        better than ever with 40% stronger <br/> bass than the
+                        better than ever with 40% stronger <br /> bass than the
                         original Mini. And the Google Assistant is a huge help
                         around the house.
                     </p>
+                    <div className="flex justify-center my-6 ">
+                        <button className="text-blue-700 hover:text-white border border-blue-800 hover:bg-blue-800  focus:outline-none focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">
+                            Buy Now
+                        </button>
+                    </div>
                 </div>
                 <video className="mx-auto" autoPlay loop muted>
                     <source src={BackgroundVideo} type="video/mp4" />
                 </video>
             </section>
             <section className="review-container">
-                <h2>Review container</h2>
+                
             </section>
-        </>
+        </div>
     );
 };
 
