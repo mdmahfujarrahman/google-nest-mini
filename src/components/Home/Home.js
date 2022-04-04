25f3f30e62c5aa910a73e4dcdc4da28150c06dbc
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-04-04 11:25:16 +0600

    extra section added.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,13 +4,16 @@ import BackgroundVideo from "../../video/background.mp4";
 import useReviews from "../../hooks/useReviews";
 import Review from "../Review/Review";
 import { Link } from "react-router-dom";
+import Idea from "../../img/idea.png";
+import IdeaSecond from "../../img/idea_1.png";
+import IdeaThird from "../../img/idea_2.png";
 
 const Home = () => {
     const [reviews, setReviews] = useReviews();
     const firstThreeReview = reviews.slice(0, 3);
     return (
         <div>
-            <section className="banner-container items-center mt-20">
+            <section className="banner-container items-center mt-36">
                 <div>
                     <h1 className="text-center text-4xl font-bold brand-color">
                         Google Nest Mini.
@@ -33,6 +36,64 @@ const Home = () => {
                 <video className="mx-auto" autoPlay loop muted>
                     <source src={BackgroundVideo} type="video/mp4" />
                 </video>
+            </section>
+            <section className="designed-idea my-20">
+                <h2 className="brand-color text-5xl text-center my-10">
+                    Thoughtfully designed.
+                </h2>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-(-25px)">
+                    <div>
+                        <img
+                            className="mx-auto rounded"
+                            src={IdeaThird}
+                            width="376px"
+                            alt=""
+                        />
+                        <div className="text-center py-6 brand-color">
+                            <h4 className="text-xl font-bold">
+                                Hang it on the wall.
+                            </h4>
+                            <p className="my-2">
+                                The Intergrated wall mount makes it easy to{" "}
+                                <br /> put in any room.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            className="mx-auto rounded"
+                            src={IdeaSecond}
+                            width="376px"
+                            alt=""
+                        />
+                        <div className="text-center py-6 brand-color">
+                            <h4 className="text-2xl font-bold">
+                                Easy volume controls.
+                            </h4>
+                            <p className="my-2">
+                                LEDs loght up to show your controls <br />
+                                when you approach it.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            className="mx-auto rounded"
+                            src={Idea}
+                            width="376px"
+                            alt=""
+                        />
+                        <div className="text-center py-6 brand-color">
+                            <h4 className="text-2xl font-bold">
+                                Sustainable materials.
+                            </h4>
+                            <p className="my-2">
+                                Favric covering made from recycled
+                                <br /> plastic bollles.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </section>
             <section className="review-container my-10">
                 <h1 className="brand-color text-5xl text-center">
